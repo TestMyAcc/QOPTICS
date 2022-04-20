1. Review computation. Make sure algorithm is correct. 2. interchange rows in plotter. 3. Add reference.

Code/python/computation.py
@@ -43,11 +43,11 @@ Gge = 0
 Geg = 0
 Epot = ( (Wx**2*grid_x**2 + Wy**2*grid_y**2 + Wz**2*grid_z**2 )
             / (2*Wz**2) )
-# circular potential
+
 psiGmu = (15*Ggg / ( 16*pi*np.sqrt(2) )  )**(2/5)  
 psiEmu = (15*Gee / ( 16*pi*np.sqrt(2) )  )**(2/5) 
 
-# psiGmu = (16*Ggg/(64*np.sqrt(2)*np.pi))**(2/5) # for oval potential
+# psiGmu = (15*Ggg/(64*np.sqrt(2)*np.pi))**(2/5) # for oval potential
 # %%
 TF_amp = np.array((psiGmu-Epot)/Ggg,dtype=np.cfloat)
 np.clip(TF_amp, 0, np.inf,out=TF_amp)
@@ -55,8 +55,8 @@ TF_pbb = np.sqrt(TF_amp)
 total = np.sum(np.abs(TF_pbb)**2*dx*dy*dz)
 n_TF_pbb = TF_pbb/np.sqrt(total)
 
-# Laguerre-Gaussian laser
 #%%
+# Laguerre-Gaussian laser
 lgpath = input("Specify filename\n"+dirutils.listLG()[1])
 lgpath = os.path.join(os.path.expanduser("~/Data/"),lgpath) + '.h5'
 if (os.path.exists(lgpath)):
@@ -110,15 +110,6 @@ def compute_BEC_Euler(_psiG, _psiE, nj):
             print(np.sum(np.abs(_psiG)**2*dx*dy*dz))
             print(np.sum(np.abs(_psiE)**2*dx*dy*dz))
     return _psiG, _psiE
-
-
-# %% 
-@njit(fastmath=True, nogil=True)
-def Hamiltonian(_psi, _G, _dx, _dy, _dz):
-    Energy = (np.sum( (np.conjugate(_psi) *  
-        (-0.5 *laplacian(_psi,dx,dy,dz)+(Epot + _G*np.abs(_psi)**2)*_psi)*dx*dy*dz)))
-
-    return Energy
 
 
 

Code/python/computation2.py
@@ -43,19 +43,21 @@ Gge = 0
 Geg = 0
 Epot = ( (Wx**2*grid_x**2 + Wy**2*grid_y**2 + Wz**2*grid_z**2 )
             / (2*Wz**2) )
-# circular potential
+
+
 psiGmu = (15*Ggg / ( 16*pi*np.sqrt(2) )  )**(2/5)  
 psiEmu = (15*Gee / ( 16*pi*np.sqrt(2) )  )**(2/5) 
 
-# psiGmu = (16*Ggg/(64*np.sqrt(2)*np.pi))**(2/5) # for oval potential
+# psiGmu = (15*Ggg/(64*np.sqrt(2)*np.pi))**(2/5) # for oval potential
 # %%
 TF_amp = np.array((psiGmu-Epot)/Ggg,dtype=np.cfloat)
 np.clip(TF_amp, 0, np.inf,out=TF_amp)
 TF_pbb = np.sqrt(TF_amp)
 total = np.sum(np.abs(TF_pbb)**2*dx*dy*dz)
 n_TF_pbb = TF_pbb/np.sqrt(total)
-# Laguerre-Gaussian laser
+
 #%%
+# Laguerre-Gaussian laser
 lgpath = input("Specify filename\n"+dirutils.listLG()[1])
 lgpath = os.path.join(os.path.expanduser("~/Data/"),lgpath) + '.h5'
 if (os.path.exists(lgpath)):
@@ -91,7 +93,7 @@ def compute_BEC_Euler(Epot:np.ndarray, psiG:np.ndarray, psiE:np.ndarray, LG:np.n
     _Lap = cp.zeros_like(psiG)
     
     
-    for j in range(nj):
+    for _ in range(nj):
         _Lap[1:Ny-1,1:Nx-1,1:Nz-1] = (
                 (0.5/dx**2)*(
                         _psiG[2:Ny,   1:Nx-1, 1:Nz-1] 
@@ -130,15 +132,8 @@ def compute_BEC_Euler(Epot:np.ndarray, psiG:np.ndarray, psiE:np.ndarray, LG:np.n
         _psiG = _psiG_n
         
         
-        if (j % stepJ) == 0:
-        #  update energy constraint 
-            Nfactor = Normalize(_psiG,_psiE,dx,dy,dz)
-            _psiGmu = _psiGmu/(Nfactor)
-            _psiEmu = _psiEmu/(Nfactor)   
-            
-    
         
-    return _psiG.get(), _psiE.get(), _psiGmu.get(), _psiEmu.get()
+    return _psiG.get(), _psiE.get()
 
 
 # %%
@@ -152,8 +147,8 @@ def compute_BEC_Euler_UpdateMu(
     _psiG_n = cp.zeros_like(_psiG)    
     _psiE = cp.array(psiE,dtype=cp.complex128)
     
-    _psiGmuArray  = cp.array(psiGmuArray, dtype=cp.float32)
-    _psiEmuArray  = cp.array(psiEmuArray, dtype=cp.float32)
+    _psiGmuArray = cp.array(psiGmuArray, dtype=cp.float32)
+    _psiEmuArray = cp.array(psiEmuArray, dtype=cp.float32)
     J = 0
     _psiGmu = _psiGmuArray[J]
     _psiEmu = _psiEmuArray[J]
@@ -223,17 +218,13 @@ def Normalize(_psiG:cp.ndarray,_psiE:cp.ndarray,_dx,_dy,_dz):
     return Nfactor
 
 
-# def Hamiltonian(_psi, _G, _dx, _dy, _dz):
-#     Energy = (np.sum( (np.conjugate(_psi) *  
-#         (-0.5 *del2(_psi,dx,dy,dz)+(Epot + _G*np.abs(_psi)**2)*_psi)*dx*dy*dz)))
 
-#     return Energy
 
 
 # %%
 import matplotlib.pyplot as plt
-nj = 100000
-stepJ = 5000
+nj = 1000000
+stepJ = 50000
 psiGmuArray = np.zeros(int(nj/stepJ),dtype=np.float32)
 psiEmuArray = np.zeros(int(nj/stepJ),dtype=np.float32)
 psiGmuArray[0] = psiGmu
@@ -299,7 +290,7 @@ plt.figure()
 plt.plot(x, np.abs(LGdata[61,:,61])**2*dx*dy*dz)
 plt.xlabel("x")
 plt.title("LG")
-# print("\n Total runs {} steps , update every {} steps\n".format(nj, stepJ))
+print("\n Total runs {} steps , update every {} steps\n".format(nj, stepJ))
 #%%
 psiG, psiE, psiGmuArray, psiEmuArray = compute_BEC_Euler_UpdateMu(
     Epot,psiG, psiE,LG,psiGmuArray, psiEmuArray,
@@ -323,6 +314,8 @@ else:
     with h5py.File(path, "w") as f:
         f['psiG'] = psiG
         f['psiE'] = psiE
+        f['psiGmuArray'] = psiGmuArray
+        f['psiEmuArray'] = psiEmuArray
         f['LGfile'] = lgpath
         f['Metaparameters/Nx'] = Nx
         f['Metaparameters/Ny'] = Ny
@@ -353,3 +346,5 @@ else:
 
 
 
+
+# %%

Code/python/plotGround.py
@@ -3,6 +3,14 @@
 import h5py
 import numpy as np
 import dirutils,os
+import del2
+
+def Hamiltonian(_psi, _G, _dx, _dy, _dz):
+     Energy = (np.sum( (np.conjugate(_psi) *  
+         (-0.5 *del2.del2(_psi,_dx,_dy,_dz)+(Epot + _G*np.abs(_psi)**2)*_psi)*_dx*_dy*_dz)))
+
+     return Energy
+
 
 base_dir = os.path.join(os.path.expanduser("~"),"Data")
 filenames = dirutils.lsfiles(base_dir) 
@@ -13,8 +21,10 @@ if not os.path.isfile(path):
 
 #%%
 with h5py.File(path, "r") as f:
-    psiG = f['psiG'][()]
-    psiE = f['psiE'][()]
+    psiG = f['psiG'][...]
+    psiE = f['psiE'][...]
+    psiGmuArray = f['psiGmuArray'][...]
+    psiEmuArray = f['psiEmuArray'][...]
     Nx = f['Metaparameters/Nx'][()]
     Ny = f['Metaparameters/Ny'][()]
     Nz = f['Metaparameters/Nz'][()]

Code/python/plotter.py
@@ -163,21 +163,21 @@ def plotdata(filepath:str, nslice:int, plotwhat='intensity'):
         
         labels = [None]*3
         Data = np.zeros((*psiG.shape,3),dtype=np.cfloat)
-        Data[...,0] = psiG
+        Data[...,0] = LG 
         Data[...,1] = psiE
-        Data[...,2] = LG
+        Data[...,2] = psiG 
         Profile = np.zeros_like(Data, dtype=float)
         
         if (plotwhat == 'intensity'):
-            Profile[...,0:2] = np.abs(Data[...,0:2])**2*dx*dy*dz #wavefunction
-            Profile[...,-1] = np.abs(Data[...,-1])**2 #Light amplitude
-            labels[0:2] = ["|psiG|^2*dx*dy*dz", "|psiE|^2*dx*dy*dz"]
-            labels[-1] =  "|LG|^2"
+            Profile[...,1:] = np.abs(Data[...,1:])**2*dx*dy*dz #wavefunction
+            Profile[...,0] = np.abs(Data[...,0])**2 #Light amplitude
+            labels[0] =  "|LG|^2"
+            labels[1:] = ["|psiE|^2*dx*dy*dz", "|psiG|^2*dx*dy*dz"]
 
         if (plotwhat == 'phase'):
             Profile[...] = np.arctan2(np.imag(Data[...]),np.real(Data[...]))
-            labels[0:2] = ["phase of psiG", "phase of psiE"]
-            labels[-1] =  "phase of LG"
+            labels[0] =  "phase of LG"
+            labels[1:] = ["phase of psiE", "phase of psiG"]
             
 
         add_slice(axs[0], z[zindice])